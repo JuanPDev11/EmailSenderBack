90f87c62f3bca75ef3a713023888eba0668e657f
JuanPDev11 <lucumi763@gmail.com>
2024-04-17 14:44:47 -0500

    add EmailSender.Web

---
+++ EmailSender.Web/Program.cs
@@ -0,0 +1,40 @@
+using EmailSender.DataAccess.Data;
+using EmailSender.DataAccess.Repositories;
+using EmailSender.DataAccess.Repositories.IRepositories;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+//DBCONTEXT
+
+builder.Services.AddDbContext<ApplicationDbContext>(opt =>
+{
+    opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+//UNIT OF WORK
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
