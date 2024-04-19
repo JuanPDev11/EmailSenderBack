b5a9f31f791959b05407c4e141ca22a8ff9147d9
JuanPDev11 <lucumi763@gmail.com>
2024-04-18 21:14:57 -0500

    add new DTOS

---
+++ EmailSender.DataAccess/DTOS/SenderDTO.cs
@@ -0,0 +1,16 @@
+﻿using EmailSender.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmailSender.DataAccess.DTOS
+{
+    public class SenderDTO
+    {
+        public Sender Sender { get; set; }
+        public IEnumerable<Sender> Senders { get; set; }
+        public Tasks TaskUnit { get; set; }
+    }
+}
---
+++ EmailSender.DataAccess/DTOS/TasksDTO.cs
@@ -0,0 +1,15 @@
+﻿using EmailSender.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmailSender.DataAccess.DTOS
+{
+    public class TasksDTO
+    {
+        public IEnumerable<Tasks> Tasks { get; set; }
+        public Tasks TasksUnit { get; set; }
+    }
+}
