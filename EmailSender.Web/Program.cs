ca739fcb803a812f3eb6394283bbba37dd2674dd
JuanPDev11 <lucumi763@gmail.com>
2024-04-19 22:55:25 -0500

    updated program.cs

--- EmailSender.Web/Program.cs
+++ EmailSender.Web/Program.cs
@@ -1,6 +1,10 @@
+using Amazon;
+using Amazon.Extensions.NETCore.Setup;
+using Amazon.Runtime;
 using EmailSender.DataAccess.Data;
 using EmailSender.DataAccess.Repositories;
 using EmailSender.DataAccess.Repositories.IRepositories;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -27,7 +31,21 @@ builder.Services.AddCors(opt=>
            .AllowAnyHeader();
     });
 });
-    
+
+////AWS S3
+AWSOptions awsOptions = new AWSOptions
+{
+    Profile = "default",
+    Region = RegionEndpoint.GetBySystemName(builder.Configuration["AWS:Region"]),
+    Credentials = new BasicAWSCredentials(builder.Configuration["AWS:AccessKey"], builder.Configuration["AWS:SecretKey"])
+};
+builder.Services.AddDefaultAWSOptions(awsOptions);
+
+
+//builder.Services.AddDefaultAWSOptions(builder.Configuration.GetAWSOptions());
+builder.Services.AddAWSService<Amazon.S3.IAmazonS3>(awsOptions);
+
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
