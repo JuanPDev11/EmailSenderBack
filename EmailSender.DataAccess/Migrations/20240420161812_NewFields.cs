0032671ebef7f948e33a15de9c0b2401f4ac3af2
JuanPDev11 <lucumi763@gmail.com>
2024-04-20 23:46:51 -0500

    add new Migrations

---
+++ EmailSender.DataAccess/Migrations/20240420161812_NewFields.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EmailSender.DataAccess.Migrations
+{
+    public partial class NewFields : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Server",
+                table: "Senders",
+                newName: "ServerSMTP");
+
+            migrationBuilder.RenameColumn(
+                name: "Port",
+                table: "Senders",
+                newName: "ServerIMAP");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateSender",
+                table: "Tasks",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Status",
+                table: "Tasks",
+                type: "bit",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "PortIMAP",
+                table: "Senders",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "PortSMTP",
+                table: "Senders",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DateSender",
+                table: "Tasks");
+
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "Tasks");
+
+            migrationBuilder.DropColumn(
+                name: "PortIMAP",
+                table: "Senders");
+
+            migrationBuilder.DropColumn(
+                name: "PortSMTP",
+                table: "Senders");
+
+            migrationBuilder.RenameColumn(
+                name: "ServerSMTP",
+                table: "Senders",
+                newName: "Server");
+
+            migrationBuilder.RenameColumn(
+                name: "ServerIMAP",
+                table: "Senders",
+                newName: "Port");
+        }
+    }
+}
---
+++ EmailSender.DataAccess/Migrations/20240421013137_AddLaterField.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EmailSender.DataAccess.Migrations
+{
+    public partial class AddLaterField : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Later",
+                table: "Tasks",
+                type: "bit",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Later",
+                table: "Tasks");
+        }
+    }
+}
