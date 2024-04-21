059b1b305eac3a906ab80a7a7fa70ceceb9e6117
JuanPDev11 <lucumi763@gmail.com>
2024-04-20 23:45:48 -0500

    add WebSiteRoles.cs

---
+++ EmailSender.Utility/WebSiteRole.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmailSender.Utility
+{
+    public class WebSiteRole
+    {
+        public const string Role_User = "User";
+        public const string Role_Super = "Supervisor";
+        public const string Role_Analist = "Analist";
+    }
+}
