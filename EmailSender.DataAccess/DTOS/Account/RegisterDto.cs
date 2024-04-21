1dd5692e3e6b78ca4765177123bdc3a5e6b10f08
JuanPDev11 <lucumi763@gmail.com>
2024-04-20 23:47:17 -0500

    add AccountDtos

---
+++ EmailSender.DataAccess/DTOS/Account/RegisterDto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmailSender.DataAccess.DTOS.Account
+{
+    public class RegisterDto
+    {
+        [Required]
+        [StringLength(15, MinimumLength = 3, ErrorMessage = "First name minimiun length {2}, adn maximun {1} characters")]
+        public string Name { get; set; }
+
+        [Required]
+        [RegularExpression("^\\w+@[a-zA-Z_]+?\\.[a-zA-Z]{2,3}$", ErrorMessage = "Invalid email address")]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(15, MinimumLength = 6, ErrorMessage = "Password name minimiun length {2}, adn maximun {1} characters")]
+        public string Password { get; set; }
+
+        public string? Role { get; set; }
+    }
+}
---
+++ EmailSender.DataAccess/DTOS/Account/UserDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmailSender.DataAccess.DTOS.Account
+{
+    public class UserDto
+    {
+        public string Name { get; set; }
+        public string JWT { get; set; }
+    }
+}
